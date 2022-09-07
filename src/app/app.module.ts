3792595fead51e1ce1387fb64d3b29ae45429554
blas <btorregrosa@infor.uva.es>
2022-09-07 11:11:09 +0200

    Creados componenetes y servicios vacios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/usuario/login/login.component';
+import { RegistroComponent } from './componentes/usuario/registro/registro.component';
+import { PerfilComponent } from './componentes/usuario/perfil/perfil.component';
+import { InicioComponent } from './componentes/usuario/inicio/inicio.component';
+import { ProfesorComponent } from './componentes/profesor/profesor/profesor.component';
+import { EstudianteComponent } from './componentes/estudiante/estudiante/estudiante.component';
+import { AdministradorComponent } from './componentes/administrador/administrador/administrador.component';
+import { NoEncontradoComponent } from './componentes/error/no-encontrado/no-encontrado.component';
+import { NoAutorizadoComponent } from './componentes/error/no-autorizado/no-autorizado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    PerfilComponent,
+    InicioComponent,
+    ProfesorComponent,
+    EstudianteComponent,
+    AdministradorComponent,
+    NoEncontradoComponent,
+    NoAutorizadoComponent
   ],
   imports: [
     BrowserModule
